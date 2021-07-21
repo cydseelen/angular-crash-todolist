b333e96cfb8a9b7a54560a6d2e7c61da5c009a69
Cydnie Seelenbinder <cydalexand@gmail.com>
2021-07-21 17:04:11 +0200

    Built Traversy Media Angular Crash Course Todo List.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
 @NgModule({
+  // All components go here, when new component created add below declarations
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    HeaderComponent,
+    AddTodoComponent,
+    AboutComponent
   ],
+  // All newly created modules need to be added below imports
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// All components have a TypeScript file that will end in .ts & not .js
---
+++ src/app/models/Todo.ts
@@ -0,0 +1,7 @@
+export class Todo {
+  id:number;
+  title:string;
+  completed:boolean;
+}
+// Fields needed for each Todo
+// Export is so that it can be brought(used) in other files
